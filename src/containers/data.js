f4ea2b14db278a4a0b9e068b3210b5ee26339092
Martin Miralles <martinmiralles66@gmail.com>
2022-04-08 10:18:31 -0400

    Initial commit and project setup

---
+++ src/containers/data.js
@@ -0,0 +1,17 @@
+export const cardData = [
+  {
+    type: "buy",
+    amount: 2000,
+    cost: 250.0,
+  },
+  {
+    type: "buy",
+    amount: 4000,
+    cost: 450.0,
+  },
+  {
+    type: "buy",
+    amount: 7000,
+    cost: 600.0,
+  },
+];
